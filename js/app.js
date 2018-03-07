3da770daf62c9ab01d47e3d39682be7cd3610e12
Jordan Archer <jordanarcherdev@gmail.com>
2018-03-07 12:07:10 +0000

    First commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let card1 = document.getElementById('cardOne');
+let card2 = document.getElementById('cardOne');
+let card3 = document.getElementById('cardOne');
+
+card3.addEventListenter('click');
